c14243e2a96c060040e79c27d8188e3449c6246a
Pizz Kun <hapizdnuryadin1412@gmail.com>
2026-05-03 21:28:52 +0700

    update pertemuan 10

---
+++ PEMROGRAMAN I/P10_HAPIZDNURYADIN/db_bimba.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 03, 2026 at 04:26 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_bimba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+-- Error reading structure for table db_bimba.kelas: #1932 - Table &#039;db_bimba.kelas&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_bimba.kelas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_bimba`.`kelas`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orang_tua`
+--
+-- Error reading structure for table db_bimba.orang_tua: #1932 - Table &#039;db_bimba.orang_tua&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_bimba.orang_tua: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_bimba`.`orang_tua`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+-- Error reading structure for table db_bimba.pembayaran: #1932 - Table &#039;db_bimba.pembayaran&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_bimba.pembayaran: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_bimba`.`pembayaran`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pendaftaran`
+--
+-- Error reading structure for table db_bimba.pendaftaran: #1932 - Table &#039;db_bimba.pendaftaran&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_bimba.pendaftaran: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_bimba`.`pendaftaran`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+-- Error reading structure for table db_bimba.siswa: #1932 - Table &#039;db_bimba.siswa&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_bimba.siswa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_bimba`.`siswa`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
